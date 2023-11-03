bd6246f50a2349667d64a8a1be44a52157253383
sadiaprova12 <sadiaprova51717@gmail.com>
2023-11-03 21:28:38 +0600

    added friend function style

--- src/App.js
+++ src/App.js
@@ -16,6 +16,9 @@ function App() {
       <Person></Person>
       <Person></Person>
       <Person></Person>
+      <h1>Cricketer Information</h1>
+      <Friend></Friend>
+      <Friend></Friend>
      {/*
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -56,14 +59,29 @@ function Person() {
   return (
     <div className='person'>
     <h1>Tamim Iqbal</h1>
-    <p>Profession: Cricket<br/>Born: Mar 20,1989 (34 years) </p>
+    <h4>Bangladesh Cricket</h4>
     <p className='profile'>When the world talks about the sub-continental openers, they talk about the aggression factor. Virender Sehwag, Sanath Jayasuriya and Sachin Tendulkar have ruthlessly destroyed the bowling over the years. However, an opener from Bangladesh soon joined that list and made the world take notice.
 
 He scored his first century against Ireland, but it was his knock against Zimbabwe at Harare in 2009 which got him top billing. Charles Coventry remained unbeaten when he equalled Saeed Anwar's highest ODI score of 194. However, Tamim launched a brutal counterattack to smash the highest score by a Bangladeshi batsman. His 154 included seven fours and six sixes as Bangladesh went on to win the match.</p>
     <h1>Shaheen Afridi</h1>
-    <p>Profession: Cricket <br/> Born: Apr 06, 2000 (23 years)</p>
+    <h4>Pakistan Cricket</h4>
     <p className='profile'>There is no denying the fact that Shaheen Afridi's potential is scaringly infinite. Blessed with a tall gangling frame and the ability to bowl really fast, Afridi also like his famous namesake can strike the ball really hard with the bat in hand. Add to all this his variety as a left-arm bowler and you know why there is immense excitement around this man in Pakistan. Born in a family of seven, Shaheen is the youngest of the lot and arguably, the most skilled as well. Despite being extremely passionate about cricket from his childhood, he was mostly into tennis-ball cricket before being guided into the real deal by his eldest brother Riaz, a one-Test wonder for Pakistan, who has also been Shaheen's inspiration and mentor.</p>
   </div>
+  )
+}
+
+function Friend(){
+  return(
+    <div className='container'>
+      <h3>Full Name: Tamim Iqbal Khan</h3>
+      <p>BORN: Mar 20, 1989 (34 years)</p>
+      <p>BATTING STYLE: Left hand Bat</p>
+      <p>PLAYING ROLE: Opening Batter</p>
+      <h3>Full Name: Shaheen Afridi</h3>
+      <p>BORN: Apr 06, 2000 (23 years)</p>
+      <p>BATTING STYLE: Left hand Bat</p>
+      <p>PLAYING ROLE: Opening Batter</p>
+    </div>
   )
 }
 
