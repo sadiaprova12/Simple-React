07d466a8a4fc30919edf8376cfa4630c47eeaebb
sadiaprova12 <sadiaprova51717@gmail.com>
2023-11-03 17:40:43 +0600

    added app js singer name

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 
+const number = 5555;
+const singer = {name: 'Atif Islam', job:', Singer'}
+const singer2 = {name: 'Eva Rahman', job:', Singer'}
+const singer3 = {name: 'Habib Wahid', job:', Singer'}
+const singerStyle= {
+  color: 'blue',
+  backgroundColor: 'yellow'
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <div>
+        <h1>Hi, Nabiha. How are you?</h1>
+        </div>
+        <div className="music">
+          <p>Number: {2222 + number}</p>
+          <div className="singer">
+          <p>Singer: {singer.name} {singer.job}</p></div>
+          <p style={singerStyle}>Singer2: {singer2.name} {singer2.job}</p>
+          <p style={{ color: 'red',
+  backgroundColor: 'white'}}>Singer3: {singer3.name} {singer3.job}</p>
+        </div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
